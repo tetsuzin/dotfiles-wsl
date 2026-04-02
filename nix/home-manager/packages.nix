0398bd0fab3250c92ffdf025cfcc0e31c3c73ee8
tetsuzin <230069+tetsuzin@users.noreply.github.com>
2026-04-02 18:19:25 +0900

    kube-ps1 のインストールを追加

--- nix/home-manager/packages.nix
+++ nix/home-manager/packages.nix
@@ -1,5 +1,19 @@
 { pkgs, ... }:
 
+let
+  kube-ps1 = pkgs.stdenv.mkDerivation {
+    name = "kube-ps1";
+    src = pkgs.fetchgit {
+      url = "https://github.com/jonmosco/kube-ps1";
+      rev = "HEAD";
+      hash = "sha256-A71FJ5o4lVa6HuSZaFIjVtjXTXN/tnS7gLkWk+A+T70=";
+    };
+    installPhase = ''
+      mkdir -p $out/share/kube-ps1
+      cp kube-ps1.sh $out/share/kube-ps1/
+    '';
+  };
+in
 {
   home.packages = with pkgs; [
     git
@@ -12,5 +26,6 @@
     # k8s
     kubectl
     kubectx
+    kube-ps1
   ];
 }
