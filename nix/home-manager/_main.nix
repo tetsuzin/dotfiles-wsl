eeb022d296c20e887240df5ca678839da32f83a2
tetsuzin <230069+tetsuzin@users.noreply.github.com>
2026-05-15 23:58:57 +0900

    .ssh/config ファイルはホストと共有にする

--- nix/home-manager/_main.nix
+++ nix/home-manager/_main.nix
@@ -18,6 +18,7 @@ in {
     # ホスト側のファイルとディレクトリにパスを貼る
     ".aws/credentials".source = mkLink "${wslHostDir}/.aws/credentials";
     ".kube/config".source = mkLink "${wslHostDir}/.kube/config";
+    ".ssh/config".source = mkLink "${wslHostDir}/.ssh/config";
   };
 
   programs.home-manager.enable = true;
